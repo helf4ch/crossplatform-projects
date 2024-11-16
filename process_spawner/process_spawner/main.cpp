6d577d780c9926350b123b0610210a1f2450aead
helf4ch <kirill24579@gmail.com>
2024-11-16 22:53:19 +1000

    feat: simple example

--- process_spawner/process_spawner/main.cpp
+++ process_spawner/process_spawner/main.cpp
@@ -1,5 +1,33 @@
 #include "libmypspawner/mypspawner.hpp"
+#include <chrono>
+#include <iostream>
+#include <thread>
 
 int main() {
+  std::chrono::milliseconds await_time(5000);
+  std::chrono::milliseconds check_time(1000);
+
+  std::string filepath =
+      "/home/helf4ch/Projects/OpSystems/process_spawner/files/repeat_hello.sh";
+  std::string pname = "awesome_repeater";
+
+  my::PSpawner spawner(filepath, pname, {}, {});
+
+  spawner.start();
+
+  std::cout << "[root] child pid: " << spawner.get_pid() << '\n';
+
+  for (int i = 0; i < await_time / check_time; ++i) {
+    std::cout << "[root] is_running: " << spawner.is_running() << '\n';
+    std::this_thread::sleep_for(check_time);
+  }
+
+  std::cout << "[root] starting wait...\n";
+
+  spawner.wait();
+
+  std::cout << "[root] wait is done\n";
+  std::cout << "[root] is_running: " << spawner.is_running() << '\n';
+
   return 0;
 }
