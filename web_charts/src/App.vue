38c05d5e924d4aed991c4d028bc8adb0a01e128e
helf4ch <kirill24579@gmail.com>
2025-01-20 13:16:32 +1000

    feat: web_chart impl

--- web_charts/src/App.vue
+++ web_charts/src/App.vue
@@ -1,32 +1,102 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted } from 'vue';
 import axios from 'axios';
 
-const message = ref('Hello World!')
-
-const items = ref([])
+const records = ref([]);
+const hours = [1, 3, 6, 12, 24];
+const selectedHours = ref(1);
 
 const fetchData = async () => {
   try {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/users')
-    items.value = response.data
+    const response = await axios.get('http://localhost:8080/get',  {
+      params: { hours: selectedHours.value },
+    });
+    records.value = response.data.reverse();
+    console.log(response.data);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 }
 
+const formatUnixTime = (unixtime) => {
+  const date = new Date(unixtime * 1000);
+  return date.toLocaleString('ru-RU');
+}
+
 onMounted(() => {
-  fetchData()
+  fetchData();
 })
 </script>
 
 <template>
   <h1>{{ message }}</h1>
   <div id="app">
-    <h1>Список данных</h1>
+    <h1>Погодные данные</h1>
+    <div>
+      <label for="hours">Период данных (в часах): </label>
+      <select id="hours" v-model="selectedHours" @change="fetchData">
+        <option v-for="hour in hours" :key="hour" :value="hour">
+          {{ hour }}
+        </option>
+      </select>
+    </div>
     <button @click="fetchData">Обновить данные</button>
-    <ul>
-      <li v-for="item in items" :key="item.id">{{ item.name }}</li>
-    </ul>
+    <table v-if="records.length" border="1">
+      <thead>
+        <tr>
+          <th>Время</th>
+          <th>Температура</th>
+          <th>Давление</th>
+          <th>Влажность</th>
+          <th>Скорость ветра</th>
+          <th>По ощущению</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="record in records" :key="record.unixtime">
+          <td>{{ formatUnixTime(record.unixtime) }}</td>
+          <td>{{ record.temperature.toFixed(1) }}</td>
+          <td>{{ record.pressure }}</td>
+          <td>{{ record.humidity }}</td>
+          <td>{{ record.wind_speed.toFixed(1) }}</td>
+          <td>{{ record.feels_like.toFixed(1) }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <p v-else>No data. Press the button.</p>
   </div>
 </template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin: 20px;
+}
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 20px;
+}
+
+th, td {
+  padding: 10px;
+  text-align: center;
+}
+
+th {
+  background-color: #f4f4f4;
+}
+
+tr:nth-child(even) {
+  background-color: #f9f9f9;
+}
+
+select {
+  margin: 10px;
+  padding: 5px;
+  font-size: 16px;
+}
+</style>
